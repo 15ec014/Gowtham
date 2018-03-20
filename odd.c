bbbce7ba8286f77753c263986491fd02086b7c87
15ec014 <gowthamsmart98@gmail.com>
2018-03-20 16:28:04 +0530

    Create odd.c

---
+++ odd.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main() {
+   int n,q;
+   scanf("%d",&n);
+   scanf("%d",&q);
+   for(int i=n+1;i<q;i++)
+   
+       if(i%2!=0)
+       {
+           printf("%d",i);
+       }
+   
+   
+}
